a5b67633552bc2b4d1c03401cfc337da2770d779
Ben Cavins <ben.cavins@flatironschool.com>
2023-01-03 16:12:30 -0500

    add category filter

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [displayItems, setDisplayItems] = useState([...items])
+
+  function onCategoryChange() {
+    const categoryFilter = document.getElementById('category-filter')
+    setDisplayItems(items.filter(item => item.category === categoryFilter.value))
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select id='category-filter' name="filter" onChange={onCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +20,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {displayItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
